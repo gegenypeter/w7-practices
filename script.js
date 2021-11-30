7dd4aaa7da943e58556a64c6453ffd3a55ecd699
GegenyPeter <gegeny.p@gmail.com>
2021-11-30 10:20:31 +0100

    .

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 /*
-function functionName(){}
-const functionName = function (){}
+Függvények metódusai
+
+
+function functionName(parameter){
+    parameter === "argument as a string";
+}
+functionName("argument as a string");
+
+
+const argument = "argument as a string";
+
+const functionName = function (paramenter){}
+functionName(argument);
+
+
 const functionName = () => {}
+functionName();
 */
-const input = () => {
+
+const input = (type) => {
+    return `
+        <input type="${type}">
     `
-    <input type="text">
-`
 }
 const form = `
     <form id="form">
         ${ input("text") }
+        ${ input("file") }
+        ${ input("email") }
+        ${ input("radio") }
+        ${ input("chackbox") }
     </form>
 `
 
 function loadEvent() {
     const root = document.getElementById("root");
-    root.insertAdjacentElement("beforeend", form);
+    root.insertAdjacentHTML("beforeend", form);
 }
 
 window.addEventListener('load', loadEvent);
